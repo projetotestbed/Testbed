4fce1daf4e4fd7b93c317550f2ffe2b866eb9bc1
dnsaoki2 <dnsaoki@gmail.com>
2015-06-30 13:22:26 -0300

    arquivos para arduino

---
+++ TBControl/files/loadnoop_3.sh
@@ -0,0 +1,12 @@
+#---------------------------------------------
+#
+#Load a binary noop file into arduino mote (type 3)
+#** It uses remote transfer file script
+#
+#---------------------------------------------
+#$1 - mote id
+#$2 - mote id
+#$3 - control channel address
+#---------------------------------------------
+./transfer.sh $3 noop_arduino.hex
+#---------------------------------------------­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­
